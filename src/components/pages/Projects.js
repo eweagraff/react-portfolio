9157e2b867df0ac011f744546b21b5bb336da0fc
eweagraff <weagraff.emily@gmail.com>
2021-08-21 19:05:27 -0700

    added alt tags to pictures

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -7,6 +7,7 @@ const Projects = () => {
           <a href="https://vasylynash.github.io/FEEL/">
             <img
               src="./assets/images/landingPage1.png"
+              alt="Feel"
               width="400"
               height="400"
             />
@@ -18,6 +19,7 @@ const Projects = () => {
           <a href="https://eweagraff.github.io/vanilla-weather-app/">
             <img
               src="./assets/images/weather_app.png"
+              alt="weather"
               width="300"
               height="200"
             />
@@ -25,12 +27,18 @@ const Projects = () => {
           <a href="https://eweagraff.github.io/Thailand_Quiz/">
             <img
               src="./assets/images/Thailandquizscreenshot.png"
+              alt="quiz"
               width="300"
               height="200"
             />
           </a>
           <a href="http://etripscav.herokuapp.com/">
-            <img src="./assets/images/tripscav.png" width="300" height="200" />
+            <img
+              src="./assets/images/tripscav.png"
+              alt="tripscav"
+              width="300"
+              height="200"
+            />
           </a>
         </div>
       </div>
