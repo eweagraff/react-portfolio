37ed3cdb114b7498a7bb49305b624d5d95582cd1
eweagraff <weagraff.emily@gmail.com>
2021-10-24 20:34:13 -0700

    added styling to buttons and added image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
 const Navbar = ({ currentPage, handlePageChange }) => {
   return (
     <nav className="navbar">
-      <h1>Emily Weagraff</h1>
+      <span className="mx-auto">Emily Weagraff</span>
+      <span className="mx-auto"> Full Stack Developer</span>
+
+      <img
+        src="./assets/images/Em.jpg"
+        alt="personal"
+        width="400px"
+        id="personal_photo"
+      />
+
       <div className="links">
         <a
           href="#projects"
@@ -10,28 +19,28 @@ const Navbar = ({ currentPage, handlePageChange }) => {
             currentPage === "Projects" ? "nav-link active" : "nav-link"
           }
         >
-          Projects
+          <button className="btn btn-lg btn-light m-2">Projects</button>
         </a>
         <a
           href="#home"
           onClick={() => handlePageChange("Home")}
           className={currentPage === "Home" ? "nav-link active" : "nav-link"}
         >
-          About Me
+          <button className="btn btn-lg btn-light m-2">About Me</button>
         </a>
         <a
           href="#contact"
           onClick={() => handlePageChange("Contact")}
           className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
         >
-          Contact
+          <button className="btn btn-lg btn-light m-2">Contact</button>
         </a>
         <a
           href="#resume"
           onClick={() => handlePageChange("Resume")}
           className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
         >
-          Resume
+          <button className="btn btn-lg btn-light m-2">Resume</button>
         </a>
       </div>
     </nav>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,21 @@
 const Home = () => {
   return (
     <div className="home">
-      <h2>About me</h2>
       <p>
-        Hi, my name is Emily. I have been learning to code for the last two
-        months. So far, I have learned the basics of HTML, CSS and JavaScript. I
-        am enjoying learning a new and valuable skill to help me change career
-        paths. In my free time I love being outside and enjoying my plants on
-        the porch!
+        Front-end web developer that has a passion for an aesthetically pleasing
+        design. I recently graduated from UC Davis Full Stack Coding Boot Camp.
+        The boot camp required 240 in-class hours and helped build a strong
+        foundation in web development. Part of the course completion included
+        building three web applications working collaboratively with a group.
+        Some of my newly developed skills include HTML/CSS, Javascript/JQuery,
+        responsive design, bootstrap, semantic UI, React.js, API interaction,
+        mongoDB and MySQL. My most recent project included utilizing React.js to
+        build a single-page web application that allowed users to search
+        National Parks, save them to a wishlist and then view them on a map. The
+        user was also required to sign up or login to search and save. As a
+        group, we shared ideas and worked as a team to complete this application
+        by utilizing Github. I am looking forward to enhancing my skill set with
+        a highly-driven team in developing skillfully designed web applications.
       </p>
     </div>
   );
